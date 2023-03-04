a370904244c2bda72822fc8f3c24502a53e679dc
ahmed wadi <ahmed.elwadi.benammar@gmail.com>
2023-03-04 13:37:59 +0100

    corriger les fautes

--- src/models/entities/Address.ts
+++ src/models/entities/Address.ts
@@ -1,12 +1,17 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { AddressTypes } from '../enums/AddressTypes';
 @Entity({ name: 'addresses' })
 export class Address {
   @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   street: string;
+  @Column()
   city: string;
+  @Column()
   country: string;
+  @Column()
   postalCode: number;
+  @Column()
   type: AddressTypes;
 }
